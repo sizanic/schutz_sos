6a800eb73ad54d3dfc02db1fceb57ccb6fe33f88
bstocker <boris.stocker@gmail.com>
2024-04-12 14:15:25 +0200

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -8,7 +8,7 @@ with open('schema2.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(1, 'Emilie', 'Victor', 2024, 10))
-cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(2 'Didier', 'Laurent', 2023, 5))
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(2, 'Didier', 'Laurent', 2023, 5))
 
 connection.commit()
 connection.close()
