f643d7b1ee7319a9b7faa8291e4c72737ddf4fee
Alssadig <165165561+Alssadig@users.noreply.github.com>
2024-07-25 17:29:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask import render_template
 from flask import json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename  
-import sqlite3
+import sqlite3 
 
 app = Flask(__name__)                                                                                                                  
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions  
