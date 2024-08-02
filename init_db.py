457422af7a0331c1e1b17e053cd93460ced1c892
sizanic <47307622+sizanic@users.noreply.github.com>
2024-08-02 03:04:13 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -3,12 +3,12 @@ import sqlite3
 connection = sqlite3.connect('database.db')
 
 # Création des tables
-with open('schema SCHUTZ.sql') as f:
+with open('Documents\schema SCHUTZ.sql') as f:
     connection.executescript(f.read())
     connection.commit()
 
 # ajout données initiales
-with open('init_db_insert.sql') as f:
+with open('Documents\init_db_insert.sql') as f:
     connection.executescript(f.read())
     connection.commit()
 
